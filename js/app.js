be7d50523fc962bdb05e5a24c580c7387d52b90d
Sirv <samuelkyleirvine@gmail.com>
2019-09-06 20:05:23 +1200

    Added error handling

--- js/app.js
+++ js/app.js
@@ -40,82 +40,110 @@ var creature_list = {
     'wolf': {num_dice:2, die_value:4, hit_mod:4, dmg_mod:2}
 };
 
+
 document.getElementById("roll_attacks_button").addEventListener("click", (e) => {
-    var total_damage = 0;
-    var adv = document.getElementById("advantage_disadvantage").value;
     var creature_string = document.getElementById("creature").value;
-    var AC = document.getElementById("target_AC").value;
-    var hits = 0;
-    var crits = 0;
-    var log = '';
-    var resistant = document.getElementById("resistance").checked;
-    var roll_1, roll_2, roll, damage;
+    var elems = document.getElementsByClassName('form-custom');
+    var invalid = false;
     if (creature_string === 'custom') {
-            var creature = {
-                num_dice:Number(document.getElementById("number_of_dice").value),
-                die_value:Number(document.getElementById("dice_value").value),
-                hit_mod:Number(document.getElementById("attack_modifier").value),
-                dmg_mod:Number(document.getElementById("damage_modifier").value)
-            };
-    } else {
-        var creature = creature_list[creature_string];
+        for (var i = 0; i < elems.length; i++) {
+            invalid = invalid || elems[i].value === '';
+        }
+        if (invalid) {
+            document.getElementById("error-custom").style.display = "block";
+        } else {
+            document.getElementById("error-custom").style.display = "none";
+        }
     }
-    log += 'Rolling ' + document.getElementById("number_of_attacks").value
-    if (adv === 'advantage') {
-        log += ' attacks with advantage\n';
-    } else if (adv === 'disadvantage') {
-        log += ' attacks with disadvantage\n';
+    if (document.getElementById("number_of_attacks").value === '') {
+        document.getElementById("error-number-attacks").style.display = "block";
+        invalid = true;
     } else {
-        log += ' attacks flat\n';
+        document.getElementById("error-number-attacks").style.display = "none";
     }
-    for (i = 0; i < Number(document.getElementById("number_of_attacks").value); i++) {
-        roll_1 = getRndInteger(1, 20);
+    if (document.getElementById("target_AC").value === '') {
+        document.getElementById("error-AC").style.display = "block";
+        invalid = true;
+    } else {
+        document.getElementById("error-AC").style.display = "none";
+    }
+    
+    if (!invalid) {
+        var total_damage = 0;
+        var adv = document.getElementById("advantage_disadvantage").value;
+        var AC = document.getElementById("target_AC").value;
+        var hits = 0;
+        var crits = 0;
+        var log = '';
+        var resistant = document.getElementById("resistance").checked;
+        var roll_1, roll_2, roll, damage;
+        if (creature_string === 'custom') {
+                var creature = {
+                    num_dice:Number(document.getElementById("number_of_dice").value),
+                    die_value:Number(document.getElementById("dice_value").value),
+                    hit_mod:Number(document.getElementById("attack_modifier").value),
+                    dmg_mod:Number(document.getElementById("damage_modifier").value)
+                };
+        } else {
+            var creature = creature_list[creature_string];
+        }
+        log += 'Rolling ' + document.getElementById("number_of_attacks").value
         if (adv === 'advantage') {
-            roll_2 = getRndInteger(1, 20);
-            roll = Math.max(roll_1, roll_2);
-            log += 'Rolled ' + roll_1.toString() + ', ' + roll_2.toString() + ', used ' + roll.toString();
+            log += ' attacks with advantage\n';
         } else if (adv === 'disadvantage') {
-            roll_2 = getRndInteger(1, 20);
-            roll = Math.min(roll_1, roll_2);
-            log += 'Rolled ' + roll_1.toString() + ' and ' + roll_2.toString() + ', used ' + roll.toString();
+            log += ' attacks with disadvantage\n';
         } else {
-            roll = roll_1;
-            log += 'Rolled ' + roll_1;
+            log += ' attacks flat\n';
         }
-        if (roll === 20) {
-            crits++;
-            hits++;
-            damage = creature.dmg_mod
-            for (j = 0; j < 2*creature.num_dice; j++) {
-                damage += getRndInteger(1, creature.die_value);
+        for (i = 0; i < Number(document.getElementById("number_of_attacks").value); i++) {
+            roll_1 = getRndInteger(1, 20);
+            if (adv === 'advantage') {
+                roll_2 = getRndInteger(1, 20);
+                roll = Math.max(roll_1, roll_2);
+                log += 'Rolled ' + roll_1.toString() + ', ' + roll_2.toString() + ', used ' + roll.toString();
+            } else if (adv === 'disadvantage') {
+                roll_2 = getRndInteger(1, 20);
+                roll = Math.min(roll_1, roll_2);
+                log += 'Rolled ' + roll_1.toString() + ' and ' + roll_2.toString() + ', used ' + roll.toString();
+            } else {
+                roll = roll_1;
+                log += 'Rolled ' + roll_1;
             }
-            if(resistant) {
-                damage = Math.floor(damage/2);
+            if (roll === 20) {
+                crits++;
+                hits++;
+                damage = creature.dmg_mod
+                for (j = 0; j < 2*creature.num_dice; j++) {
+                    damage += getRndInteger(1, creature.die_value);
+                }
+                if(resistant) {
+                    damage = Math.floor(damage/2);
+                }
+                log += ', crit! Does ' + damage.toString() + ' damage.\n'
+            } else if (roll == 1) {
+                damage = 0;
+                log += ', total miss.\n'
+            } else if (roll + creature.hit_mod >= AC) {
+                hits++;
+                damage = creature.dmg_mod
+                for (j = 0; j < creature.num_dice; j++) {
+                    damage += getRndInteger(1, creature.die_value); 
+                }
+                if(resistant) {
+                    damage = Math.floor(damage/2);
+                }
+                log += ', hit. Does ' + damage.toString() + ' damage.\n'
+            } else {
+                damage = 0;
+                log += ', miss.\n'
             }
-            log += ', crit! Does ' + damage.toString() + ' damage.\n'
-        } else if (roll == 1) {
-            damage = 0;
-            log += ', total miss.\n'
-        } else if (roll + creature.hit_mod >= AC) {
-            hits++;
-            damage = creature.dmg_mod
-            for (j = 0; j < creature.num_dice; j++) {
-                damage += getRndInteger(1, creature.die_value); 
-            }
-            if(resistant) {
-                damage = Math.floor(damage/2);
-            }
-            log += ', hit. Does ' + damage.toString() + ' damage.\n'
-        } else {
-            damage = 0;
-            log += ', miss.\n'
+            total_damage += damage;
         }
-        total_damage += damage;
+        document.getElementById("damage").innerHTML = total_damage;
+        document.getElementById("num_hits").innerHTML = hits;
+        document.getElementById("num_crits").innerHTML = crits;
+        document.getElementById("log").innerHTML = log;
     }
-    document.getElementById("damage").innerHTML = total_damage;
-    document.getElementById("num_hits").innerHTML = hits;
-    document.getElementById("num_crits").innerHTML = crits;
-    document.getElementById("log").innerHTML = log;
 });
 
 if ('serviceWorker' in navigator) {
